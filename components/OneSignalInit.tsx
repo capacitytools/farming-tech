f7b1937d572a9b83e6569d6172dd2102a1a110dd
capacitytools <capacitydotbuild@gmail.com>
2026-08-11 08:37:34 -0700

    Update OneSignalInit.tsx

--- components/OneSignalInit.tsx
+++ components/OneSignalInit.tsx
@@ -3,13 +3,15 @@
 import { useEffect } from "react";
 
 const ONE_SIGNAL_APP_ID = "2873e8a6-070e-4bc4-91cb-123c7cd1c0ef";
+const SAFARI_WEB_ID = "web.onesignal.auto.1881b8be-1ae3-4d80-a99d-32f491a07c07";
 
 export default function OneSignalInit() {
   useEffect(() => {
     window.OneSignalDeferred = window.OneSignalDeferred || [];
-    window.OneSignalDeferred.push(function (OneSignal: any) {
-      OneSignal.init({
+    window.OneSignalDeferred.push(async function (OneSignal: any) {
+      await OneSignal.init({
         appId: ONE_SIGNAL_APP_ID,
+        safari_web_id: SAFARI_WEB_ID, // This makes it work on iPhones!
         allowLocalhostAsSecureOrigin: true,
         serviceWorkerParam: { scope: "/" },
         serviceWorkerPath: "/OneSignalSDKWorker.js",
@@ -20,6 +22,11 @@ export default function OneSignalInit() {
           size: "medium",
           theme: "default",
           showCredit: false,
+          text: {
+            "tip.state.unsubscribed": "🔔 Subscribe to updates",
+            "tip.state.subscribed": "✅ You're subscribed!",
+            "tip.state.blocked": "⚠️ Notifications blocked",
+          },
         },
       });
     });
